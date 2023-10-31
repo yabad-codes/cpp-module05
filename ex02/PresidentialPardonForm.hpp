a000c29299cd8b5154e35d39d8f0c2c5ff77a114
Yahya ABAD <yabad@student.1337.ma>
2023-10-31 22:11:25 +0100

    creating Presidential class

---
+++ ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabad <yabad@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 22:08:24 by yabad             #+#    #+#             */
+/*   Updated: 2023/10/31 22:10:48 by yabad            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRESIDENTIALPARDONFORM_HPP
+# define PRESIDENTIALPARDONFORM_HPP
+
+# include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm {
+	private:
+		std::string	target;
+	public:
+		PresidentialPardonForm();
+		PresidentialPardonForm(std::string target);
+		PresidentialPardonForm(const PresidentialPardonForm&);
+		~PresidentialPardonForm();
+		
+		PresidentialPardonForm&	operator=(const PresidentialPardonForm&);
+		
+		void	executeAction() const;
+};
+
+#endif
