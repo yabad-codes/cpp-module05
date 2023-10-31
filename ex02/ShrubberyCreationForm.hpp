adb02eb7ec0101c9792603860ca06a8968b4eb25
Yahya ABAD <yabad@student.1337.ma>
2023-10-31 19:31:44 +0100

    adding Shrubbery class with basic canonical methods

---
+++ ex02/ShrubberyCreationForm.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabad <yabad@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 19:21:01 by yabad             #+#    #+#             */
+/*   Updated: 2023/10/31 19:30:44 by yabad            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHRUBBERYCREATIONFORM_HPP
+# define SHRUBBERYCREATIONFORM_HPP
+
+# include "AForm.hpp"
+
+class ShrubberyCreationForm : public AForm {
+	private:
+		std::string	target;
+	public:
+		ShrubberyCreationForm();
+		ShrubberyCreationForm(std::string target);
+		ShrubberyCreationForm(const ShrubberyCreationForm&);
+		~ShrubberyCreationForm();
+
+		ShrubberyCreationForm&	operator=(const ShrubberyCreationForm&);
+};
+
+#endif
