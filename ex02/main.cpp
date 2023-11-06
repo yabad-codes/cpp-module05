da343a922b68886d8be3068a76f6338d0d94eadf
Yahya Abad <yabad@c2r2p9.1337.ma>
2023-11-06 15:38:02 +0100

    making the tests look more clear

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yabad <yabad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 14:20:55 by yabad             #+#    #+#             */
-/*   Updated: 2023/10/31 22:29:16 by yabad            ###   ########.fr       */
+/*   Updated: 2023/11/06 15:34:33 by yabad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,35 @@
 #include "PresidentialPardonForm.hpp"
 
 int	main() {
-	// {
-	// 	Bureaucrat	b1("Cap", 69);
-	// 	Bureaucrat	b2("Spiderman", 130);
-	// 	ShrubberyCreationForm	form;
+	std::cout << "*------------------ Start shrubbery testing block ------------------*" << std::endl;
+	{
+		Bureaucrat	b1("Cap", 69);
+		Bureaucrat	b2("Spiderman", 130);
+		ShrubberyCreationForm	form;
 
-	// 	try {
-	// 		form.beSigned(b1);
-	// 		form.execute(b2);
-	// 	} catch(std::exception& e) {
-	// 		std::cerr << e.what() << std::endl;
-	// 	}
-	// }
-	// {
-	// 	Bureaucrat	b1("Cap", 69);
-	// 	Bureaucrat	b2("Spiderman", 46);
-	// 	RobotomyRequestForm	form;
+		try {
+			form.beSigned(b1);
+			form.execute(b2);
+		} catch(std::exception& e) {
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	std::cout << "*------------------ End shrubbery testing block ------------------*" << std::endl << std::endl;
+	std::cout << "*------------------ Start robotomy testing block ------------------*" << std::endl;
+	{
+		Bureaucrat	b1("Cap", 69);
+		Bureaucrat	b2("Spiderman", 45);
+		RobotomyRequestForm	form;
 
-	// 	try {
-	// 		form.beSigned(b1);
-	// 		form.execute(b2);
-	// 	} catch(std::exception& e) {
-	// 		std::cerr << e.what() << std::endl;
-	// 	}
-	// }
+		try {
+			form.beSigned(b1);
+			form.execute(b2);
+		} catch(std::exception& e) {
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	std::cout << "*------------------ End robotomy testing block ------------------*" << std::endl << std::endl;
+	std::cout << "*------------------ Start presedential pardon testing block ------------------*" << std::endl;
 	{
 		Bureaucrat	b1("Cap", 3);
 		Bureaucrat	b2("Spiderman", 24);
@@ -52,4 +57,5 @@ int	main() {
 			std::cerr << e.what() << std::endl;
 		}
 	}
+	std::cout << "*------------------ End presidential pardon testing block ------------------*" << std::endl << std::endl;
 }
