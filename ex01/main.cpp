3b239274fc58a7ff50abd32e90d6a4ee78b547f6
Yahya Abad <yabad@c2r2p9.1337.ma>
2023-11-06 14:18:37 +0100

    replacing printf c function with cout

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yabad <yabad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 14:20:55 by yabad             #+#    #+#             */
-/*   Updated: 2023/10/31 16:11:09 by yabad            ###   ########.fr       */
+/*   Updated: 2023/11/06 14:17:56 by yabad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main() {
 	try {
 		form.beSigned(b1);
 		b1.signForm(form);
-		printf("Should not be printed on the screen");
+		std::cout << "Should not be printed on the screen" << std::endl;
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
